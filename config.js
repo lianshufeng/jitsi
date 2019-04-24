97acbaeeff585e552e0bff923427bc75aa10443b
lianshufeng <251708339@qq.com>
2019-04-24 10:50:43 +0800

    update

--- config.js
+++ config.js
@@ -1,15 +1,19 @@
+//载入工具类
+let fs = require('fs'), path = require('path');
+
+let mePath = path.dirname(path.dirname(__filename));
+
 module.exports = {
 
     // git 更新后的路径
     path: {
-        "jitsi-meet":"E:/github/jitsi-meet",
-        "lib-jitsi-meet":"E:/github/lib-jitsi-meet",
+        "jitsi-meet": path.join(mePath, "jitsi-meet"),
+        "lib-jitsi-meet": path.join(mePath, "lib-jitsi-meet"),
     },
-
     // 设置默认的帧率
     defaultFrameRate: 40,
 
     //隐藏水印
-    hideWatermark:true
+    hideWatermark: true
 
 }
